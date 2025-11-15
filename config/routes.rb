94d41f8a4c45719b3d80f3b333481f63adf3c62c
yuyuyu-63 <yukino1999713@gmail.com>
2025-11-16 04:02:18 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
